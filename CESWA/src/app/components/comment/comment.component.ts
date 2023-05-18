d8d914545dbf845f19475e77fe05a0fa0b266e5b
HAMZA KARACA <hkaraca19@posta.pau.edu.tr>
2023-05-18 10:34:16 +0300

    last

--- CESWA/src/app/components/comment/comment.component.ts
+++ CESWA/src/app/components/comment/comment.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommentModel } from 'src/app/models/commentModel';
+import { AuthService } from 'src/app/services/auth.service';
 import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
 })
-export class CommentComponent {
-  private commentService:CommentService
-  comments:Comment[];
-  getComment(){
-    this.commentService.getAllComment(this.comments);
+export class CommentComponent implements OnInit {
+  id:number
+  ngOnInit(): void {
+    this.getComment();
+  }
+  constructor(public commentService: CommentService,private authService:AuthService) {}
+  comments: CommentModel[];
+
+  getComment() {
+    this.commentService.getAllComment(this.comments).subscribe(
+      (next) => (this.comments = next.data),
+      (err) => null
+    );
+  }
+  
+  deleteByYourSelf(){
+    this.commentService.deleteCommentByYourself(id)
+    
   }
 }
